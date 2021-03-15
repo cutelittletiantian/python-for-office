e5e45910c2bba91f59e7c7b5bc78fabf449eceec
cutelittletiantian <619591789@qq.com>
2021-03-15 22:22:40 +0800

    Three review examples in req.7 are renamed.

---
+++ 需求7：Pdf文件读取数据处理/review7_2.py
@@ -0,0 +1,14 @@
+import os
+
+pdfListPath = "./resources/review7_2"
+
+pdfDocs = os.listdir(pdfListPath)
+
+
+def sort_pdf(pdf_file):
+    pdf_name = os.path.splitext(p=pdf_file)[0]
+    return int(pdf_name)
+
+
+pdfDocs.sort(key=sort_pdf, reverse=False)
+print(pdfDocs)
---
+++ 需求7：Pdf文件读取数据处理/review7_3.py
@@ -0,0 +1,31 @@
+# review7_1 用非函数方法实现
+import os
+
+docPath = "./resources/review7_3"
+
+allItems = os.listdir(docPath)
+
+
+chapterList = []
+
+for item in allItems:
+    itemName = os.path.split(p=item)[0]
+    chapterCode = itemName.split(sep="-")[1]
+    chapterList.append(chapterCode)
+
+# 执行排序
+chapterList.sort(key=int)
+
+# 空白列表
+file = []
+# 还原文件名
+for chapter in chapterList:
+    for item in allItems:
+        newChapter = str(chapter)
+        sourceName = item.split(sep="-")[1]
+        newName = sourceName.split(sep=".")[0]
+
+        if newChapter == newName:
+            file.append(item)
+
+print(file)
